600d9a0409a2c859f71d380ec8bc22d6f9b70f0f
Nagasatya Sai Nallam <n.nagasatyasai@gmail.com>
2023-08-31 21:02:54 +0530

    front section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors : {
+        'darkvs' : "#242424",
+        "darkfull": "#25252b"
+      }
+    },
+  },
+  plugins: [],
+}
